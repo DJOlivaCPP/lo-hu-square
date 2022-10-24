d4bf307ea63e6771b24355417386b465148c8358
Dylan Oliva <djoliva@cpp.edu>
2022-10-23 20:39:55 -0700

    outline

---
+++ LoHuSquare.c
@@ -0,0 +1,9 @@
+//2 files:
+//one file to store the table values
+//one file containing the logic for the square
+
+//create two arrays to test functions if the functions are correct
+//we add vertically and howrizontally for each row
+
+//create a new random table to test the functions
+//loop until a Lo Hu Square is created
